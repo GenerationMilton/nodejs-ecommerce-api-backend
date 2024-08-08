a29d01cd19903ee4f8568666e14c7fe5e6c87bbd
Milton Muñoz <livemilton@gmail.com>
2024-08-07 20:03:06 -0500

    filter products by Colors, Brands, sizes

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -52,8 +52,40 @@ export const getProductsController = asyncHandler(async(req, res)=>{
         });
     }
 
-      //await the query
-      const products= await productQuery;
+    //filter by brand
+
+    if(req.query.brand){
+        productQuery=productQuery.find({
+            brand:{$regex: req.query.brand, $options:"i"},
+        });
+    }
+
+    //filter by category
+
+    if(req.query.category){
+        productQuery=productQuery.find({
+            category:{$regex: req.query.category, $options:"i"},
+        });
+    }
+
+     //filter by color
+
+     if(req.query.color){
+        productQuery=productQuery.find({
+            colors:{$regex: req.query.color, $options:"i"},
+        });
+    }
+
+    //filter by sizes
+
+    if(req.query.size){
+    productQuery=productQuery.find({
+        sizes:{$regex: req.query.size, $options:"i"},
+    });
+    }
+
+    //await the query
+    const products= await productQuery;
 
     res.json({
         status:"success",
